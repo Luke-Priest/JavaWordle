33cab1dec7f6e1661f7e6d4f01e784b2e5c3ab45
scr4pmechanic <sjamesmm@gmail.com>
2025-05-06 08:28:45 -0400

    new bot file

---
+++ CoolerBot.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DuhBot implements Bot {
+
+    // variables and stuff here
+
+    /**
+    * Generates a guess by selecting the next word in the solution dictionary.
+    *
+    * @param puzzle the puzzle to try and solve
+    *
+    * @return the next word in the solution dictionary
+    */
+    public String attempt(Wordle puzzle) {
+        // code here that guesses the word
+    }
+}
